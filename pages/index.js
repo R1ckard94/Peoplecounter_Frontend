da66c82fa0bd7a14e0aa2f793654b84d71afbda2
Rika1700 <78212506+Rika1700@users.noreply.github.com>
2022-03-25 14:30:22 +0100

    lite updates

--- pages/index.js
+++ pages/index.js
@@ -7,26 +7,32 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <header>
-        <title>People Counter</title>
-      </header>
-      <h1 className={styles.MainText} >People Counter på Atea!</h1>
-      <main className={styles.main}>
-      
-        <div className={styles.card}>
-        
-        </div>
-        <div className={styles.card}>
-        
-        </div>
-      </main>
+    <div classname='mainHolder'>
+      <div className={styles.sidePanel}>
 
-      <footer className={styles.footer}>
-        <a>
-          
-        </a>
-      </footer>
+      </div>
+      <div className={styles.container}>
+        <header>
+          <title>People Counter</title>
+        </header>
+        <h1 className={styles.MainText} >People Counter på Atea!</h1>
+
+        <main className={styles.main}>
+
+          <div className={styles.card}>
+
+          </div>
+          <div className={styles.card}>
+
+          </div>
+        </main>
+
+        <footer className={styles.footer}>
+          <a>
+
+          </a>
+        </footer>
+      </div>
     </div>
   )
 }
