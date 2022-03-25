3cb68b99f1a169692e64f73502242005af62bd54
Rika1700 <78212506+Rika1700@users.noreply.github.com>
2022-03-25 14:24:12 +0100

    lite updates

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,24 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
+
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <header>
         <title>People Counter</title>
       </header>
-
+      <h1 className={styles.MainText} >People Counter på Atea!</h1>
       <main className={styles.main}>
-        <h1>Välkommen till People counter på Atea</h1>
+      
+        <div className={styles.card}>
+        
+        </div>
+        <div className={styles.card}>
+        
+        </div>
       </main>
 
       <footer className={styles.footer}>
